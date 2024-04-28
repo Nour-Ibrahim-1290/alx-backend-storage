04d0d363524bf79dd7337a28a397466b63953f1e
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-28 07:59:50 +0300

    sql-5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+--- email validation sent
+
+
+DROP TRIGGER IF EXISTS reset_valid_email;
+DELIMITER $$
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
